323515501d647c79bd88209b5aa013a59e5b2bc1
ChatKozak <138544516+ChatKozak@users.noreply.github.com>
2023-08-26 23:17:44 +0530

    Update gpu_job.sh

--- gpu_job.sh
+++ gpu_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=my_gpu_job
 #SBATCH --output=output.txt
 #SBATCH --error=error.txt
-#SBATCH --partition=gpu_partition  # Change this to your GPU partition
+#SBATCH --partition=gpu  # Use the 'gpu' partition
 #SBATCH --gres=gpu:1  # Request 1 GPU
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
@@ -11,7 +11,7 @@
 
 # Load any necessary modules or environment variables for your GPU setup
 # For example, loading CUDA if your script requires it
-module load cuda
+# module load cuda  # Uncomment this line if CUDA is required
 
 # Navigate to the directory containing your RML.py script
 cd /path/to/your/script/directory
