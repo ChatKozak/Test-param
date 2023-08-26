145623ad2b790ac1bc439a6dd9b21f6c85a3aab1
ChatKozak <138544516+ChatKozak@users.noreply.github.com>
2023-08-26 23:26:38 +0530

    Update gpu_job.sh

--- gpu_job.sh
+++ gpu_job.sh
@@ -6,15 +6,15 @@
 #SBATCH --gres=gpu:1  # Request 1 GPU
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=4  # Adjust this based on your requirements
+#SBATCH --cpus-per-task=8  # Adjust this based on your requirements
 #SBATCH --time=00:10:00  # Adjust this to your desired runtime
 
 # Load any necessary modules or environment variables for your GPU setup
 # For example, loading CUDA if your script requires it
-# module load cuda  # Uncomment this line if CUDA is required
+module load cuda  # Uncomment this line if CUDA is required
 
 # Navigate to the directory containing your RML.py script
-cd /path/to/your/script/directory
+cd /home/dai002/KoZaK/Test-param
 
 # Run your Python script
 python RML.py
