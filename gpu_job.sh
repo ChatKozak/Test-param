18277aab7844c6c235a40c306ba0e55f4cb87beb
ChatKozak <138544516+ChatKozak@users.noreply.github.com>
2023-08-26 19:27:35 +0530

    Create gpu_job.sh

---
+++ gpu_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=my_gpu_job
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+#SBATCH --partition=gpu_partition  # Change this to your GPU partition
+#SBATCH --gres=gpu:1  # Request 1 GPU
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=4  # Adjust this based on your requirements
+#SBATCH --time=00:10:00  # Adjust this to your desired runtime
+
+# Load any necessary modules or environment variables for your GPU setup
+# For example, loading CUDA if your script requires it
+module load cuda
+
+# Navigate to the directory containing your RML.py script
+cd /path/to/your/script/directory
+
+# Run your Python script
+python RML.py
